5cbd2b7ff5ffed8951fc60c316f82e5b3a89ebce
hephaestic <hephaestic@gmail.com>
2016-08-06 23:09:02 -0700

    all good

---
+++ server.js
@@ -0,0 +1,19 @@
+var app = require('express')();
+var port = process.env.PORT || 3000;
+var parser= require('ua-parser-js');
+
+app.listen(port, function(){
+  console.log('Listening on port ' + port);
+});
+
+app.get('/', function(req, res){
+  var ip = req.ip;
+  var language = req.get("accept-language").split(',')[0];
+  var os = parser(req.get('User-Agent')).os;
+  var software = os.name + ' ' + os.version;
+  res.json({
+    "ipaddress": ip,
+    "language": language,
+    "software": software
+  });
+})
